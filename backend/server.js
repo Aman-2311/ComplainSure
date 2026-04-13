84c88c5e85d3c2eb050903a999715e2e46b983ea
Aman07 <135688213+Aman-2311@users.noreply.github.com>
2026-04-13 11:07:10 +0530

    Move index.html to project root directory

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use('/api/admin',     adminRoutes);
 
 // fallback to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // start server
